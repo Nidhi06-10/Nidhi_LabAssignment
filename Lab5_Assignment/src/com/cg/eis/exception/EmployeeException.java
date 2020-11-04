e992b1d9e2bb08c350000e5ba78396c02aff6d70
Nidhi06-10 <nidhinishmitha06@gmail.com>
2020-11-03 20:53:02 -0600

    Assignment

---
+++ Lab5_Assignment/src/com/cg/eis/exception/EmployeeException.java
@@ -0,0 +1,29 @@
+package com.cg.eis.exception;
+
+/**
+ * 
+ * Author :Nidhi Desc:An employeeException class is created
+ */
+public class EmployeeException {
+	private float salary;
+
+	/**
+	 * to get the salary of the employee
+	 * 
+	 * @return salary
+	 */
+
+	public float getSalary() {
+		return salary;
+	}
+
+	/**
+	 * to set the salary
+	 * 
+	 * @param salary
+	 */
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+}
